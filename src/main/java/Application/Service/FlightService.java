6ef8c6ba39d47e9887bb8f853eb4249cc4e21b14
Damonlh <myorderrecipt@gmail.com>
2023-02-03 19:09:38 +0000

    Commit

--- src/main/java/Application/Service/FlightService.java
+++ src/main/java/Application/Service/FlightService.java
@@ -5,6 +5,8 @@ import Application.DAO.FlightDAO;
 
 import java.util.List;
 
+import org.junit.internal.runners.statements.FailOnTimeout;
+
 /**
  * The purpose of a Service class is to contain "business logic" that sits between the web layer (controller) and
  * persistence layer (DAO). That means that the Service class performs tasks that aren't done through the web or
@@ -53,7 +55,8 @@ public class FlightService {
      *         inform our provide the front-end client with information about the added Flight.
      */
     public Flight addFlight(Flight flight){
-        return null;
+        
+        return flightDAO.insertFlight(flight);
     }
 
     /**
@@ -70,6 +73,14 @@ public class FlightService {
      *         user should have some insight if they attempted to edit a nonexistent flight.)
      */
     public Flight updateFlight(int flight_id, Flight flight){
+
+        if (flightDAO.getFlightById(flight_id) != null)
+        {
+            Flight updatedFlight = new Flight(flight_id, flight.departure_city, flight.arrival_city);
+            
+            return updatedFlight;
+        }
+
         return null;
     }
 
@@ -80,7 +91,8 @@ public class FlightService {
      * @return all flights in the database.
      */
     public List<Flight> getAllFlights() {
-        return null;
+        
+        return flightDAO.getAllFlights();
     }
 
     /**
@@ -92,6 +104,7 @@ public class FlightService {
      * @return all flights departing from departure_city and arriving at arrival_city.
      */
     public List<Flight> getAllFlightsFromCityToCity(String departure_city, String arrival_city) {
-        return null;
+
+        return flightDAO.getAllFlightsFromCityToCity(departure_city, arrival_city);
     }
 }
